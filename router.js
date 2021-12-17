a6c52653f75fa4f4651a0aeaf5898a789a3636a3
Aanaoui <othmane.developpeur@gmail.com>
2021-12-17 17:04:02 +0100

    first

---
+++ router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+const service = require('./service/userService');
+const User = require('./models/user');
+const lobby = require('./models/lobby');
+
+router.get('/user', service.getUsers);
+router.get('/lobby', service.getLobbies);
+
+router.use('/', (req, res, next) => {
+    if (req.url == '/') {
+        res.redirect('/user');
+    } else {
+        next();
+    }
+});
+
+router.get('/user/:id', service.getById);
+
+router.delete('/user/:id', service.delete);
+router.post('/user', service.add);
+router.put('/user', service.update);
+router.use((req, res) => {
+    res.status(404).send('Page non trouvée');
+});
+
+module.exports = router;
